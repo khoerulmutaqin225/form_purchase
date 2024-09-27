52967529181ed10860fdebfc4b6ceb31cc052708
khoerulmutaqin225 <khoerulmutaqin225@gmail.com>
2024-09-27 11:47:43 +0700

    remove file un use

--- models/pos_order.py
+++ models/pos_order.py
@@ -18,35 +18,3 @@ class PosOrder(models.Model):
             if email_to:
                 mail_template.email_to = email_to
         mail_template.send_mail(self.id, force_send=True)
-        
-        
-        
-         
-         
-        
-
-    @api.model
-    def send_email_receipt(self, order):
-        if order.partner_id and order.partner_id.email:
-            template = self.env.ref('form_purchase.email_template_receipt')
-            template.send_mail(order.id, force_send=True)
-            return "Email sudah dikirim / Email sent"
-        else:
-            raise UserError("Customer must have an email address.")
-
-    def _get_order_fields(self):
-        fields = super(PosOrder, self)._get_order_fields()
-        fields.remove('price_total')  # Disable Price
-        fields.remove('discount')  # Disable Discount
-        return fields
-    
-    @api.model
-    def send_email_receipt(self, order):
-        if order.partner_id and order.partner_id.email:
-            template = self.env.ref('form_purchase.email_template_receipt')
-            template.send_mail(order.id, force_send=True)
-            return "Email sudah dikirim / Email sent"
-        else:
-            raise UserError("Customer must have an email address.")
-
-
